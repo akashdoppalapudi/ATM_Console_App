4400345fe30544ab52790b9aadb0d1abf4e35c54
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-09-26 23:45:15 +0530

    Add project files.

---
+++ ATM/Account.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ATM
+{
+    [Serializable]
+    class Account
+    {
+        public string name;
+        public int accountNumber;
+        private int availBal, pin;
+        private List<string> transactions;
+
+        public Account(int accNo)
+        {
+            Console.WriteLine("\n\n____CREATING ACCOUNT____");
+            Console.Write("Enter Your name : ");
+            this.name = Console.ReadLine();
+            Console.Write("Set a 4-digit pin : ");
+            string selectedPin = Console.ReadLine();
+            if (selectedPin.Length != 4)
+            {
+                Console.WriteLine("Invalid Pin! Try again");
+                this.pin = -1;
+            }
+            else
+            {
+                try
+                {
+                    this.pin = Convert.ToInt32(selectedPin);
+                    this.accountNumber = accNo;
+                    this.availBal = 0;
+                    this.transactions = new List<string>();
+                    string transaction = DateTime.Now.ToString() + "\tAccount Created";
+                    transactions.Add(transaction);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid Pin! Try again");
+                    this.pin = -1;
+                }
+            }
+        }
+
+        public bool isPinValid()
+        {
+            if(this.pin == -1)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        public void deposit()
+        {
+            Console.Write("\nEnter PIN : ");
+            string enteredPin = Console.ReadLine();
+            if (enteredPin == Convert.ToString(this.pin))
+            {
+                Console.WriteLine("\n\n____DEPOSIT____");
+                Console.Write("Enter ammount to be deposited : ");
+                int ammount = Convert.ToInt32(Console.ReadLine());
+                if (ammount > 0)
+                {
+                    this.availBal += ammount;
+                    string transaction = DateTime.Now.ToString() + "\tCredited    Rs. " + Convert.ToString(ammount);
+                    this.transactions.Add(transaction);
+                    Console.WriteLine("Ammount Deposited Successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Ammount");
+                }
+            } else
+            {
+                Console.WriteLine("Wrong pin!");
+            }
+            
+        }
+
+        public void withdraw()
+        {
+            Console.Write("\nEnter PIN : ");
+            string enteredPin = Console.ReadLine();
+            if (enteredPin == Convert.ToString(this.pin))
+            {
+                Console.WriteLine("\n\n____WITHDRAW____");
+                Console.Write("Enter ammount to be withdrawn : ");
+                int ammount = Convert.ToInt32(Console.ReadLine());
+                if (ammount > 0 && ammount <= this.availBal)
+                {
+                    this.availBal -= ammount;
+                    string transaction = DateTime.Now.ToString() + "\tDebited     Rs. " + Convert.ToString(ammount);
+                    this.transactions.Add(transaction);
+                    Console.WriteLine("Ammount Withdrawn Successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Amount");
+                }
+            } else
+            {
+                Console.WriteLine("Wrong Pin!");
+            }
+            
+        }
+
+        public int Tout()
+        {
+            Console.Write("\nEnter PIN : ");
+            string enteredPin = Console.ReadLine();
+            if (enteredPin == Convert.ToString(this.pin))
+            {
+                Console.WriteLine("\n\n____TRANSFER____");
+                Console.Write("Enter ammount to be transfered : ");
+                int ammount = Convert.ToInt32(Console.ReadLine());
+                if (ammount > 0 && ammount <= this.availBal)
+                {
+                    this.availBal -= ammount;
+                    string transaction = DateTime.Now.ToString() + "\tDebited     Rs. " + Convert.ToString(ammount);
+                    this.transactions.Add(transaction);
+                    return ammount;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Amount");
+                    return -1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong Pin!");
+                return -1;
+            }
+        }
+
+        public void Tin(int amt)
+        {
+            this.availBal += amt;
+            string transaction = DateTime.Now.ToString() + "\tCredited    Rs. " + Convert.ToString(amt);
+            this.transactions.Add(transaction);
+            Console.WriteLine("Ammount Transfered Successfully");
+        }
+
+        public void history()
+        {
+            Console.Write("\nEnter PIN : ");
+            string enteredPin = Console.ReadLine();
+            if (enteredPin == Convert.ToString(this.pin))
+            {
+                Console.WriteLine("\n\n____TRANSACTION HISTORY____");
+                foreach (string transaction in transactions)
+                {
+                    Console.WriteLine(transaction);
+                }
+                Console.WriteLine("\t\tAvailable Balance : Rs. " + this.availBal);
+            }
+            else
+            {
+                Console.WriteLine("Wrong Pin!");
+            }
+        }
+    }
+}
---
+++ ATM/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ATM
+{
+    class Program
+    {
+        public const string FILEPATH = "../../../../accountsInfo.dat";
+        public static BinaryFormatter formatter = new BinaryFormatter();
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                List<Account> accounts = readAccounts();
+                Console.WriteLine("\n\n____ATM____");
+                Console.WriteLine("1. Create New Account\n2. Exixting User");
+                Console.Write("Select an Option (Enter 'e' to exit) : ");
+                string option = Console.ReadLine();
+                if(option == "1")
+                {
+                    Account newAcc = new Account(accounts.Count+1);
+                    if (newAcc.isPinValid())
+                    {
+                        accounts.Add(newAcc);
+                        writeAccounts(accounts);
+                    } else
+                    {
+                        newAcc = null;
+                    }
+                    
+                } else if (option == "2")
+                {
+                    while(true)
+                    {
+                        Console.WriteLine("\n\n____EXISTING ACCOUNTS____");
+                        foreach (Account acc in accounts)
+                        {
+                            Console.WriteLine(acc.accountNumber + ". " + acc.name);
+                        }
+                        Console.Write("Select your Account (Enter 'b' to go back) : ");
+                        string selectedAcc = Console.ReadLine();
+                        int index;
+                        try
+                        {
+                            index = Convert.ToInt32(selectedAcc) - 1;
+                        } catch (Exception e)
+                        {
+                            if (selectedAcc != "b")
+                            {
+                                Console.WriteLine("Invalid Option!");
+                                continue;
+                            } else
+                            {
+                                break;
+                            }
+                        }
+                        if (index >= 0 && index < accounts.Count)
+                        {
+                            Console.WriteLine("\n\n____OPERATIONS____");
+                            Console.WriteLine("\n1. Deposit\n2. Withdraw\n3. Transfer\n4. Show Transaction History");
+                            Console.Write("Select an operation (Enter 'b' to go back) : ");
+                            string operation = Console.ReadLine();
+                            if (operation=="1")
+                            {
+                                accounts[index].deposit();
+                                break;
+                            } else if (operation=="2")
+                            {
+                                accounts[index].withdraw();
+                                break;
+                            } else if (operation=="3")
+                            {
+                                int amt = accounts[index].Tout();
+                                Console.WriteLine();
+                                foreach (Account acc in accounts)
+                                {
+                                    if (accounts[index].accountNumber != acc.accountNumber)
+                                    {
+                                        Console.WriteLine(acc.accountNumber + ". " + acc.name);
+                                    }
+                                }
+                                Console.Write("Select an Account to transfer money to : ");
+                                string choice = (Console.ReadLine());
+                                try
+                                {
+                                    int transferTo = Convert.ToInt32(choice);
+                                    if (amt > 0)
+                                    {
+                                        accounts[transferTo - 1].Tin(amt);
+                                        break;
+                                    } else
+                                    {
+                                        Console.WriteLine("Transfer Failed");
+                                        break;
+                                    }
+                                } catch (Exception e)
+                                {
+                                    Console.WriteLine("Invalid Choice!");
+                                    continue;
+                                }
+                            } else if (operation=="4") {
+                                accounts[index].history();
+                                break;
+                            } else if (operation=="b")
+                            {
+                                continue;
+                            } else
+                            {
+                                Console.WriteLine("invalid Choice");
+                                break;
+                            }
+                        } else
+                        {
+                            Console.WriteLine("Invalid Option!");
+                        }
+
+                        writeAccounts(accounts);
+                    }
+                } else if (option == "e")
+                {
+                    Environment.Exit(0);
+                } else
+                {
+                    Console.WriteLine("Invalid Option!");
+                }
+            }
+        }
+
+        public static List<Account> readAccounts()
+        {
+            if (File.Exists(FILEPATH))
+            {
+                FileStream readerFileStream = new FileStream(FILEPATH, FileMode.Open, FileAccess.Read);
+                List<Account> accounts = (List<Account>)formatter.Deserialize(readerFileStream);
+                readerFileStream.Close();
+                return accounts;
+            } else
+            {
+                List<Account> accounts = new List<Account>();
+                return accounts;
+            }
+        }
+
+        public static void writeAccounts(List<Account> accounts)
+        {
+            FileStream writerFileStream = new FileStream(FILEPATH, FileMode.Create, FileAccess.Write);
+            formatter.Serialize(writerFileStream, accounts);
+            writerFileStream.Close();
+        }
+    }
+}
