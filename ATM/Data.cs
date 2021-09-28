42a51788399c89cd55015bae6a00a443bb877f65
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-09-28 23:05:37 +0530

    added Data Class for handling data stored in local memory

---
+++ ATM/Data.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace ATM
+{
+    public class Data
+    {
+        private const string FILEPATH = "../../../../accountsInfo.dat";
+        public static BinaryFormatter formatter = new BinaryFormatter();
+
+        public static List<Account> readAccounts()
+        {
+            if (File.Exists(FILEPATH))
+            {
+                FileStream readerFileStream = new FileStream(FILEPATH, FileMode.Open, FileAccess.Read);
+                List<Account> accounts = (List<Account>)formatter.Deserialize(readerFileStream);
+                readerFileStream.Close();
+                return accounts;
+            }
+            else
+            {
+                List<Account> accounts = new List<Account>();
+                return accounts;
+            }
+        }
+
+        public static void writeAccounts(List<Account> accounts)
+        {
+            FileStream writerFileStream = new FileStream(FILEPATH, FileMode.Create, FileAccess.Write);
+            formatter.Serialize(writerFileStream, accounts);
+            writerFileStream.Close();
+        }
+    }
+}
