5287ae241aba590ec8f64fa5e38c2c0377707354
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-10-06 15:25:27 +0530

    Added Exceptions

---
+++ ATM.Services/ExceptionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace ATM.Services
+{
+    public class AccountCreationFailedException : Exception
+    {
+    }
+
+    public class InvalidAmountException : Exception
+    {
+    }
+
+    public class AuthenticationFailedException : Exception
+    {
+    }
+
+    public class TransferFailedException : Exception
+    {
+    }
+}
