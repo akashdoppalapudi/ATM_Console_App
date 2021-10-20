430535893edef651ad703289e91d3257778ecc3a
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-10-20 22:35:16 +0530

    Added Person Model
    Created a Person Model and Inherited it to Account and Employee Model.

--- ATM.Models/Employee.cs
+++ ATM.Models/Employee.cs
@@ -4,13 +4,8 @@ using System;
 namespace ATM.Models
 {
     [Serializable]
-    public class Employee
+    public class Employee: Person
     {
-        public string Id { get; set; }
-        public string Username { get; set; }
-        public string Name { get; set; }
-        public Gender Gender { get; set; }
         public EmployeeType EmployeeType { get; set; }
-        public string Password { get; set; }
     }
 }
