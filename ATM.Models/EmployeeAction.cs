2276218a4529a4c044021aa84ab4d7a81d4a51a1
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-10-21 12:14:48 +0530

    Created EmployeeAction Model
    Created EmployeeAction Model with Several properties and also created the EmployeeActionType Enum

---
+++ ATM.Models/EmployeeAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ATM.Models.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Models
+{
+    public class EmployeeAction
+    {
+        public string Id { get; set; }
+        public string TXNId { get; set; }
+        public string AccountId { get; set; }
+        public DateTime ActionDate { get; set; }
+        public EmployeeActionType ActionType { get; set; }
+    }
+}
---
+++ ATM.Models/Enums/EmployeeActionType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Models.Enums
+{
+    public enum EmployeeActionType
+    {
+        NewAccount = 1,
+        UpdateAccount,
+        DeleteAccount,
+        ServiceChargeUpdate,
+        RevertTransaction
+    }
+}
