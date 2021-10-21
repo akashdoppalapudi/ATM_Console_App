9be2a153f7081ac873ffadd0ac3acc6fc0299dbb
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-10-21 12:17:39 +0530

    Added Employee Actions List to Employee model

--- ATM.Models/EmployeeAction.cs
+++ ATM.Models/EmployeeAction.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using ATM.Models.Enums;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ATM.Models.Enums;
+using System;
 
 namespace ATM.Models
 {
