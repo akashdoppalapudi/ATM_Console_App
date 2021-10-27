85d524f0cc5230a3809237083f5571cbf5645bc2
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-10-27 16:01:56 +0530

    Ran Code Cleanup

--- ATM.Models/Employee.cs
+++ ATM.Models/Employee.cs
@@ -1,11 +1,11 @@
 ﻿using ATM.Models.Enums;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 
 namespace ATM.Models
 {
     [Serializable]
-    public class Employee: Person
+    public class Employee : Person
     {
         public EmployeeType EmployeeType { get; set; }
         public List<EmployeeAction> EmployeeActions { get; set; }
