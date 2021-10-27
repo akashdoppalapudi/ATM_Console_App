5e54eb752fe84f62f7c451fd7bc9c69953f33bc1
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-10-27 16:35:31 +0530

    added IMPS,RTGS to bank model and set their default values at initialization

--- ATM.Models/Bank.cs
+++ ATM.Models/Bank.cs
@@ -11,6 +11,10 @@ namespace ATM.Models
         public List<Account> Accounts { get; set; }
         public List<Employee> Employees { get; set; }
         public List<Currency> Currencies { get; set; }
+        public double IMPS { get; set; }
+        public double RTGS { get; set; }
+        public double OIMPS { get; set; }
+        public double ORTGS { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
